74a7fcb3f5ae0f0e82eb67b24e5f0d230e40c92f
Brian Lechthaler <brianlechthaler@protonmail.ch>
2020-09-19 18:01:02 -0700

    fix custom_ls.sh

--- bin/custom_ls.sh
+++ bin/custom_ls.sh
@@ -3,6 +3,6 @@ echo "Usage: custom_ls.sh <username> <password> <host>"
 echo "Username: $1"
 echo "Password: <REDACTED>"
 echo "ES Host: $3"
-sudo sed 's/your_elasticsearch_user/$1/g'
-sudo sed 's/your_elasticsearch_password/"$2"/g'
-sudo sed 's/your_elasticsearch_host/"$3"/g'
+sudo sed 's/your_elasticsearch_user/$1/g' /opt/tpot/etc/custom-ls/logstash.conf
+sudo sed 's/your_elasticsearch_password/"$2"/g' /opt/tpot/etc/custom-ls/logstash.conf
+sudo sed 's/your_elasticsearch_host/"$3"/g' /opt/tpot/etc/custom-ls/logstash.conf
