1c202653033a74934b7780c443f8404034597f36
Brian Lechthaler <brianlechthaler@protonmail.ch>
2020-09-22 14:45:53 -0700

    significantly improve custom_ls.sh
    * interactively ask user for password instead of using a command line argument that would leave credentials in your shell's history, for example `.bash_history`
    * Check if user is running this script as root, and produce an error if the user is not running this as root.
    * ensure permissions on config file
    * talk to the user a lot more

--- bin/custom_ls.sh
+++ bin/custom_ls.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
-echo "Usage: custom_ls.sh <username> <password> <host>"
+echo "Usage: custom_ls.sh <username> <host> "
 echo "Username: $1"
-echo "Password: <REDACTED>"
-echo "ES Host: $3"
-sudo sed 's/your_elasticsearch_user/$1/g' /opt/tpot/etc/custom-ls/logstash.conf
-sudo sed 's/your_elasticsearch_password/"$2"/g' /opt/tpot/etc/custom-ls/logstash.conf
-sudo sed 's/your_elasticsearch_host/"$3"/g' /opt/tpot/etc/custom-ls/logstash.conf
+echo "ES Host: $2"
+
+export cfg="/opt/tpot/etc/custom-ls/logstash.conf"
+echo "Configuration directory: $cfg"
+
+if $USER="root"
+then
+  read -s -p "Type or paste in the password for $1 and hit ENTER: " password
+  sed 's/your_elasticsearch_password/"$password"/g' $cfg
+  unset password
+  sed 's/your_elasticsearch_user/$1/g' $cfg
+  sed 's/your_elasticsearch_host/"$2"/g' $cfg
+  chown tpot:tpot $cfg
+  echo "Success: Configuration updated. To restart Logstash, try:"
+  echo "sudo docker restart logstash"
+  echo "IMPORTANT: Be sure to check container log output to make sure the new configuration does not produce any errors."
+else
+  echo "Permission Error: It looks like you are not root. Please try running this command again with sudo."
+  
